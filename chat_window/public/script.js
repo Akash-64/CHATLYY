61c9cfbc69437f6c51befbf9e13336c7aa9033d9
AKASH . <138816822+Akash-64@users.noreply.github.com>
2025-04-02 21:58:46 +0530

    Add files via upload

---
+++ chat_window/public/script.js
@@ -0,0 +1,38 @@
+const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+
+const name = prompt('What is your name?');
+appendMessage('You joined', 'right');
+socket.emit('new-user', name);
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`, 'left');
+});
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`, 'left');
+});
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`, 'left');
+});
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value.trim();
+    if (message !== '') {
+        appendMessage(`You: ${message}`, 'right'); // Your messages appear on the right
+        socket.emit('send-chat-message', message);
+        messageInput.value = '';
+    }
+});
+
+function appendMessage(message, position) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message', position);
+    messageContainer.appendChild(messageElement);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
